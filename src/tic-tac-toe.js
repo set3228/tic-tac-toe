1e8354a0e11de6a19521dd7425560a8a0eebfe3b
set3228 <2012ynwa@gmail.com>
2017-09-28 23:13:18 +0300

    tic-tac-toe solution was added

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,76 @@
 class TicTacToe {
     constructor() {
-
+        this.game = [null, null, null, null, null, null, null, null, null];
+        this.turn = 0;
+        this.winner = null;
     }
 
     getCurrentPlayerSymbol() {
-
+        if(this.turn % 2) return 'o';
+        return 'x';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(!(this.game[rowIndex * 3 + columnIndex])) {
+            this.game[rowIndex * 3 + columnIndex] = this.getCurrentPlayerSymbol();
+            this.turn += 1;
+        }
+        this.isFinished();
     }
 
     isFinished() {
+        /* Проверяем 3 в строке */
+        for(var i = 0; i < 3; i++) {
+            if(this.game[i * 3] === this.game[i * 3 + 1]
+                && this.game[i * 3] === this.game[i * 3 + 2]
+                && this.game[i * 3] !== null) {
+                    this.winner = this.game[i * 3];
+                    return true;
+                }
+        }
+        /* Проверяем 3 в столбце */
+        for(i = 0; i < 3; i++) {
+            if(this.game[i] === this.game[i + 3]
+               && this.game[i] === this.game[i + 6]
+               && this.game[i] !== null) {
+                   this.winner = this.game[i];
+                   return true;
+               }
+        }
+        /* Проверяем 3 по диагонали */
+        if(this.game[0] === this.game[4]
+            && this.game[0] === this.game[8]
+            && this.game[0] !== null) {
+                this.winner = this.game[0];
+                return true;
+            }
+        if(this.game[2] === this.game[4]
+            && this.game[2] === this.game[6]
+            && this.game[2] !== null) {
+                this.winner = this.game[2];
+                return true;
+            }
+        /* Проверяем количество ходов */
+        if(this.turn == 9) return true;
 
+        return false;
     }
 
     getWinner() {
-
+        return this.winner;
     }
 
     noMoreTurns() {
-
+        return this.turn == 9;
     }
 
     isDraw() {
-
+        if(this.turn == 9 && !this.winner) return true;
+        return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.game[rowIndex * 3 + colIndex];
     }
 }
 
